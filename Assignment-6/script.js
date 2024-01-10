980f8198d6331bfbbac57276c3143f4cef807df1
Hafsa <135526388+HafsaSaeed313@users.noreply.github.com>
2024-01-10 10:42:06 -0800

    Add files via upload

---
+++ Assignment-6/script.js
@@ -0,0 +1,12 @@
+//Math Expression//
+
+var Number1 = parseInt(prompt("Enter First Number"));
+var Number2 = parseInt(prompt("Enter Second Number"));
+
+document.write("The Sum is " + (Number1 + Number2) + "<br>");
+
+//concatination//
+
+var userName = prompt("Please Enter your Name!");
+document.write("Welcome " + userName);
+3
